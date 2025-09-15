8b4e349a512fee763eafbd9651d0426885260ba8
hadi3169 <moazzenhadi46@gmail.com>
2025-09-15 22:04:36 +0330

    add no stock files   new update 1404-6-24

--- sidebar.php
+++ sidebar.php
@@ -35,7 +35,7 @@ $menu_items = [
     [
         'url' => 'Statistics.php',
         'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#2563eb"><path fill="currentColor" d="M3 17h2v-7H3v7zm4 0h2V7H7v10zm4 0h2v-4h-2v4zm4 0h2V4h-2v13zm4 0h2V9h-2v8z"/></svg>',
-        'text' => 'آمار'
+        'text' => ' گزارشات و آمار'
     ],
 
 ];
